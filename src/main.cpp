19189d100988f293ffbde44b561f2eb50ee3d356
Bonemind <Subhime@gmail.com>
2017-01-30 16:44:15 +0100

    Added comments to code

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,37 @@
 #define IR_SENSOR_PIN_PIN PIND
 #define IR_SENSOR_PIN 11
 #define LEDpin 7
+#define FEEDBACKPIN 10
+#define CAMERA_CONTROL_PIN 9
 
+// The last state the ir-led was in, true if visible, false if not visible
 bool lastState = false;
-const int FEEDBACKPIN = 10;
-const int CAMERA_CONTROL_PIN = 9;
 
+// The camera object, used to control the camera
 Nikon nikon(CAMERA_CONTROL_PIN);
 
+// Initial applcation setup
 void setup(void) {
+	// Setup serial communication
 	Serial.begin(9600);
-	Serial.println("Ready to decode IR!");
+
+	// Feedback
+	Serial.println("Ready");
+
+	// Set pins in correct modes
 	pinMode(LEDpin, OUTPUT);
 	pinMode(IR_SENSOR_PIN, INPUT);
 	pinMode(FEEDBACKPIN, OUTPUT);
 	pinMode(CAMERA_CONTROL_PIN, OUTPUT);
+
+	// Set pins to low, just so we have a known start state
 	digitalWrite(LEDpin, LOW);
 	digitalWrite(CAMERA_CONTROL_PIN, LOW);
 	digitalWrite(FEEDBACKPIN, LOW);
 	digitalWrite(IR_SENSOR_PIN, LOW);
 }
 
+// Puls led for 38khz reciever
 void IR38Write() {
 	for(int i = 0; i <= 384; i++) {
 		digitalWrite(LEDpin, HIGH);
@@ -37,22 +48,30 @@ void IR38Write() {
 	}
 }
 
+// Meat and bones of the application
 void loop(void) {
+	// Pulse the led so the reciever can detect it
 	IR38Write();
+
+	// Read the led state, if low then the led is visible
 	bool currState = digitalRead(IR_SENSOR_PIN) == LOW;
+
+	// The led just became visible, or invisible
+	// Trigger camera shutter
 	if (currState != lastState) {
-		Serial.print("---------------------\n");
-		Serial.print("Change\n");
 		nikon.shutterNow();
 		if (currState) {
-			Serial.print("Now on\n");
+			Serial.print("On");
 			digitalWrite(FEEDBACKPIN, HIGH);
 		} else {
-			Serial.print("Now off\n");
+			Serial.print("Off");
 			digitalWrite(FEEDBACKPIN, LOW);
 		}
 	}
+
+	// Store last state
 	lastState = currState;
+
 	delay(50);
 }
 
