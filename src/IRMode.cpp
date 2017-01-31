86ebc62957ce30b0896fa4a13b8dd9e02acf707e
Bonemind <Subhime@gmail.com>
2017-01-31 18:53:34 +0100

    Basis for IRMode

---
+++ src/IRMode.cpp
@@ -0,0 +1,64 @@
+#include "IRMode.h"
+
+
+IRMode::IRMode(Nikon* cam) {
+	this->camera = cam;
+	this->lastState = false;
+}
+
+/**
+ * Initialize the ir mode
+ */
+char *IRMode::getName() {
+	return "IR";
+}
+
+/**
+ * Initialize the ir mode
+ */
+void IRMode::initialize() {
+	//Void
+}
+
+/**
+ * Update the ir mode
+ * No-op since the mode is not doing anything
+ */
+void IRMode::update() {
+	// Pulse the led so the reciever can detect it
+	this->pulseIR();
+
+	// Read the led state, if low then the led is visible
+	bool currState = digitalRead(IR_SENSOR_PIN) == LOW;
+
+	// The led just became visible, or invisible
+	// Trigger camera shutter
+	if (currState != lastState) {
+		if (currState) {
+			Serial.print("On");
+		} else {
+			Serial.print("Off");
+		}
+	}
+
+	// Store last state
+	lastState = currState;
+}
+
+// Puls led for 38khz reciever
+void IRMode::pulseIR() {
+	for(int i = 0; i <= 384; i++) {
+		digitalWrite(IRMODE_LED_PIN, HIGH);
+		delayMicroseconds(13);
+		digitalWrite(IRMODE_LED_PIN, LOW);
+		delayMicroseconds(13);
+	}
+}
+
+/**
+ * Cleanup after running the ir mode
+ * There is nothing to clean up, so no-op
+ */
+void IRMode::end() {
+	//void
+}
---
+++ src/IRMode.h
@@ -0,0 +1,40 @@
+#ifndef IRMode_h
+#define IRMode_h
+
+#include "Mode.h"
+
+/**
+ * Defines the ir mode as a child of Mode
+ */
+class IRMode : public Mode
+{
+	public:
+		IRMode(Nikon* cam);
+		/**
+		 * Startup of the mode
+		 */
+		char *getName();
+
+		/**
+		 * Startup of the mode
+		 */
+		void initialize();
+
+		/**
+		 * Update during mode running
+		 */
+		void update();
+
+		/**
+		 * Cleanup after the mode was completed
+		 */
+		void end();
+
+	private:
+		Nikon* camera;
+		bool lastState;
+
+		void pulseIR();
+};
+
+#endif
